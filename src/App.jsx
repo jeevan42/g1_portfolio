199e0c37b3c797a4df5e93a8d87d2bfa4a934b2a
jeevan42 <114385490+jeevan42@users.noreply.github.com>
2023-12-28 05:22:14 +0000

    Added My  initial details

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Jeevan Kumar",
+  title: "Web Developer",
+  email: "g1kumar05092002@gmail.com",
+  gitHub: "https://github.com/jeevan42",
+  instagram: "https://www.instagram.com/iamjeevan.k/",
+  linkedIn: "https://www.linkedin.com/in/jeevan-kumar-g14202/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
